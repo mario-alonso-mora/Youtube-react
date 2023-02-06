db14381b3b579032ac988f3a84a828ab9971c62e
Mario <marioalonsomora84@gmail.com>
2023-02-06 18:10:56 +0100

    completed react-youtube

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,9 +16,10 @@ export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
 export const categories = [
     { name: 'New', icon: <HomeIcon />, },
-    { name: 'JS Mastery', icon: <CodeIcon />, },
     { name: 'Coding', icon: <CodeIcon />, },
     { name: 'ReactJS', icon: <CodeIcon />, },
+    { name: 'VueJS', icon: <CodeIcon />, },
+    { name: 'NodeJS', icon: <CodeIcon />, },
     { name: 'NextJS', icon: <CodeIcon />, },
     { name: 'Music', icon: <MusicNoteIcon /> },
     { name: 'Education', icon: <SchoolIcon />, },
